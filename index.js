2e2d9312f2f2caccd8cb725e9a96087dcb4c67d4
M.Nisar Ali <101400298+MNisarAli@users.noreply.github.com>
2022-12-22 11:11:37 +0500

    Task 3 Test case passed

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ stringLength = (string) => {
   throw new Error("string length should be 1-10 char");
 };
 
-const reverseString = (string) => {
+reverseString = (string) => {
   return string.split("").reverse().join("");
 }
 
-module.exports = { stringLength, reverseString };
+class Calculator {
+  add = (num1, num2) => num1 + num2;
+  subtract = (num1, num2) => num1 - num2;
+  divide = (num1, num2) => num1 / num2;
+  multiply = (num1, num2) => num1 * num2;
+}
+
+
+module.exports = { stringLength, reverseString, Calculator };
--- index.test.js
+++ index.test.js
@@ -1,4 +1,5 @@
-const { stringLength, reverseString } = require("./index.js");
+const { stringLength, reverseString, Calculator } = require("./index.js");
+const calculator = new Calculator;
 
 // Task 1 => Test Case 1
 test("Should output string character count", () => {
@@ -13,7 +14,68 @@ test('Check whether string length is between 1-10', () => {
 });
 
 // Task 2
-test("Should output reversed string", () =>{
+test("Should output reversed string", () => {
   const output = reverseString("string");
   expect(output).not.toBe("string");
+});
+
+// Task 3 => Test Case 1
+describe("Check Add Calculator", () => {
+  test("Should retrun sum of numbers", () => {
+    expect(calculator.add(5, 5)).toBe(10);
+  });
+
+  test("Should retrun sum of numbers", () => {
+    expect(calculator.add(5, -20)).not.toBe(10);
+  });
+
+  test("Should retrun sum of numbers", () => {
+    expect(calculator.add(5, 20)).toBe(25);
+  });
+});
+
+// Task 3 => Test Case 2
+describe("Check Subtract Calculator", () => {
+  test("Should retrun subtraction result", () => {
+    expect(calculator.subtract(5, 5)).toBe(0);
+  });
+
+  test("Should retrun subtraction result", () => {
+    expect(calculator.subtract(5, -20)).not.toBe(10);
+  });
+
+  test("Should retrun subtraction result", () => {
+    expect(calculator.subtract(5, 20)).toBe(-15);
+  });
+});
+
+// Task 3 => Test Case 3
+describe("Check Divide Calculator", () => {
+  test("Should retrun division result", () => {
+    expect(calculator.divide(5, 5)).toBe(1);
+  });
+
+  test("Should retrun division result", () => {
+    expect(calculator.divide(5, -20)).not.toBe(10);
+  });
+
+  test("Should retrun division result", () => {
+    expect(calculator.divide(27, 3)).toBe(9);
+  });
+});
+
+
+// Task 3 => Test Case 4
+describe("Check Multiply Calculator", () => {
+  test("Should retrun multiplication result", () => {
+    expect(calculator.multiply(5, 5)).toBe(25);
+  });
+
+  test("Should retrun multiplication result", () => {
+    expect(calculator.multiply(5, -20)).not.toBe(10);
+  });
+
+  test("Should retrun multiplication result", () => {
+    expect(calculator.multiply(27, 3)).toBe(81);
+  });
 });
